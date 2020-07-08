53526bddda12e9e508972728de0916059e371614
Wayne Van Son <waynevanson@gmail.com>
2020-07-08 19:04:40 +1000

    feat: fchmod overloads

--- src/fchmod.ts
+++ src/fchmod.ts
@@ -1,26 +1,47 @@
-/**
- * @since 0.0.0
- */
 import { taskEither } from "fp-ts";
 import * as fs from "fs";
 import { enforceErrnoException, FileDescriptor } from "./util";
 import { FilePermissions } from "./util/types/file-permissions";
-import { ReaderTaskEitherNode } from "./util/types/fp";
+import { TaskEitherNode, ReaderTaskEitherNode } from "./util/types/fp";
+import { _chown } from "./chown";
+import { _chmod } from "./chmod";
 
-/**
- * @since 0.0.0
- */
+export function _fchmod(
+  fileDescriptor: FileDescriptor,
+  mode: FilePermissions
+): TaskEitherNode {
+  return taskEither.tryCatch(
+    () =>
+      new Promise<void>((resolve, reject) => {
+        fs.fchmod(fileDescriptor, mode, (e) => {
+          !e ? resolve() : reject(e);
+        });
+      }),
+    enforceErrnoException
+  );
+}
+
 export function fchmod(
-  permissions: FilePermissions
-): ReaderTaskEitherNode<FileDescriptor> {
-  return (fileDescriptor) =>
-    taskEither.tryCatch(
-      () =>
-        new Promise<void>((resolve, reject) => {
-          fs.fchmod(fileDescriptor, permissions, (e) => {
-            !e ? resolve() : reject(e);
-          });
-        }),
-      enforceErrnoException
-    );
+  mode: FilePermissions
+): ReaderTaskEitherNode<FileDescriptor>;
+
+export function fchmod(
+  fileDescriptor: FileDescriptor,
+  mode: FilePermissions
+): TaskEitherNode;
+
+export function fchmod(
+  a: FilePermissions | FileDescriptor,
+  b?: FilePermissions
+) {
+  // first overload
+  if (b === undefined) {
+    const mode = a as FilePermissions;
+    return (fileDescriptor: FileDescriptor) => _fchmod(fileDescriptor, mode);
+  }
+
+  // second overload
+  const fileDescriptor = a as FileDescriptor;
+  const mode = b as FilePermissions;
+  return _fchmod(fileDescriptor, mode);
 }
