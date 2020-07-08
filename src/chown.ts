928db8cdb158d23fa2ab48cb0a0da871f12736f8
Wayne Van Son <waynevanson@gmail.com>
2020-07-08 18:56:51 +1000

    feat: chown overloads

--- src/chown.ts
+++ src/chown.ts
@@ -1,23 +1,46 @@
 import { taskEither } from "fp-ts";
 import * as fs from "fs";
 import { enforceErrnoException } from "./util";
-import { FilePermissions } from "./util/types/file-permissions";
-import { ReaderTaskEitherNode } from "./util/types/fp";
+import { ReaderTaskEitherNode, TaskEitherNode } from "./util/types/fp";
+
+export function _chown(
+  pathLike: fs.PathLike,
+  uid: number,
+  gid: number
+): TaskEitherNode {
+  return taskEither.tryCatch(
+    () =>
+      new Promise<void>((resolve, reject) => {
+        fs.chown(pathLike, uid, gid, (e) => (!e ? resolve() : reject(e)));
+      }),
+    enforceErrnoException
+  );
+}
+
+export function chown(
+  pathLike: fs.PathLike,
+  uid: number,
+  gid: number
+): TaskEitherNode;
+
+export function chown(
+  uid: number,
+  gid: number
+): ReaderTaskEitherNode<fs.PathLike, void>;
 
 /**
  * @summary
  * Changes the owner ship of a file.
  */
-export function chown(
-  uid: FilePermissions,
-  gid: FilePermissions
-): ReaderTaskEitherNode<fs.PathLike, void> {
-  return (path) =>
-    taskEither.tryCatch(
-      () =>
-        new Promise<void>((resolve, reject) => {
-          fs.chown(path, uid, gid, (e) => (!e ? resolve() : reject(e)));
-        }),
-      enforceErrnoException
-    );
+export function chown(a: fs.PathLike | number, b: number, c?: number) {
+  if (c === undefined) {
+    const uid = a as number;
+    const gid = b as number;
+    return (pathLike: fs.PathLike) => _chown(pathLike, uid, gid);
+  }
+
+  const pathLike = a as fs.PathLike;
+  const uid = b as number;
+  const gid = c as number;
+  return _chown(pathLike, uid, gid);
 }
