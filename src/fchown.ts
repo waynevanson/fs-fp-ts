943e9155b5c518eb3d6c122c88bcf9b65f5ab420
Wayne Van Son <waynevanson@gmail.com>
2020-07-08 19:32:24 +1000

    feat: adds fchown overloads

--- src/fchown.ts
+++ src/fchown.ts
@@ -1,26 +1,47 @@
-/**
- * @since 0.0.0
- */
 import { taskEither } from "fp-ts";
 import * as fs from "fs";
 import { enforceErrnoException, FileDescriptor } from "./util";
-import { ReaderTaskEitherNode } from "./util/types/fp";
+import { ReaderTaskEitherNode, TaskEitherNode } from "./util/types/fp";
 
-/**
- * @since 0.0.0
- */
+export function _fchown(
+  pathLike: FileDescriptor,
+  uid: number,
+  gid: number
+): TaskEitherNode {
+  return taskEither.tryCatch(
+    () =>
+      new Promise<void>((resolve, reject) => {
+        fs.fchown(pathLike, uid, gid, (e) => (!e ? resolve() : reject(e)));
+      }),
+    enforceErrnoException
+  );
+}
+
+export function fchown(
+  pathLike: FileDescriptor,
+  uid: number,
+  gid: number
+): TaskEitherNode;
+
 export function fchown(
   uid: number,
   gid: number
-): ReaderTaskEitherNode<FileDescriptor> {
-  return (fileDescriptor) =>
-    taskEither.tryCatch(
-      () =>
-        new Promise<void>((resolve, reject) => {
-          fs.fchown(fileDescriptor, uid, gid, (e) =>
-            !e ? resolve() : reject(e)
-          );
-        }),
-      enforceErrnoException
-    );
+): ReaderTaskEitherNode<FileDescriptor, void>;
+
+/**
+ * @summary
+ * Changes the owner ship of a file.
+ */
+export function fchown(a: FileDescriptor, b: number, c?: number) {
+  if (c === undefined) {
+    const uid = a as number;
+    const gid = b as number;
+    return (fileDescriptor: FileDescriptor) =>
+      _fchown(fileDescriptor, uid, gid);
+  }
+
+  const fileDescriptor = a as FileDescriptor;
+  const uid = b as number;
+  const gid = c as number;
+  return _fchown(fileDescriptor, uid, gid);
 }
