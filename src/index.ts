953da9dc76b147371a4ae1b36964ce9e80c24d15
Wayne Van Son <waynevanson@gmail.com>
2020-07-09 08:51:56 +1000

    feat add overloaded mkdtemp

--- src/index.ts
+++ src/index.ts
@@ -17,3 +17,4 @@ export { link } from "./link";
 export { lstat } from "./lstat";
 export { lutimes } from "./lutimes";
 export { mkdir } from "./mkdir";
+export { mkdtemp, BufferEncodingOrBuffer } from "./mkdtemp";
--- src/mkdtemp.ts
+++ src/mkdtemp.ts
@@ -1,25 +1,51 @@
-import { taskEither } from "fp-ts";
+import { taskEither as TE } from "fp-ts";
 import * as fs from "fs";
 import { enforceErrnoException } from "./util";
-import { TaskEither } from "fp-ts/lib/TaskEither";
+import { TaskEitherNode } from "./util/types/fp";
 
-export function mkdtemp<O extends "buffer">(
-  options: O
-): (prefix: string) => TaskEither<NodeJS.ErrnoException, Buffer>;
+export function _mkdtemp(prefix: string, as: BufferEncodingOrBuffer) {
+  const encoding = as === "Buffer" ? null : as;
+  return TE.tryCatch(
+    () =>
+      new Promise<string | Buffer>((resolve, reject) => {
+        fs.mkdtemp(prefix, encoding, (e, folder) =>
+          !e ? resolve(folder) : reject(e)
+        );
+      }),
+    enforceErrnoException
+  );
+}
 
-export function mkdtemp<O extends BufferEncoding>(
-  options: O
-): (prefix: string) => TaskEither<NodeJS.ErrnoException, string>;
+export type BufferEncodingOrBuffer = BufferEncoding | "Buffer";
 
-export function mkdtemp<O extends BufferEncoding | "buffer">(options: O) {
-  return (prefix: string) =>
-    taskEither.tryCatch(
-      () =>
-        new Promise<string | Buffer>((resolve, reject) => {
-          fs.mkdtemp(prefix, options, (e, folder) =>
-            !e ? resolve(folder) : reject(e)
-          );
-        }),
-      enforceErrnoException
-    );
+export type CreateBufferOrString<
+  T extends BufferEncodingOrBuffer
+> = T extends "Buffer" ? Buffer : string;
+
+export function mkdtemp<T extends BufferEncodingOrBuffer>(
+  as?: BufferEncoding | "Buffer"
+): (prefix: string) => TaskEitherNode<CreateBufferOrString<T>>;
+
+export function mkdtemp<T extends BufferEncodingOrBuffer>(
+  prefix: string,
+  as?: BufferEncoding | "Buffer"
+): TaskEitherNode<CreateBufferOrString<T>>;
+
+export function mkdtemp<T extends BufferEncodingOrBuffer = "Buffer">(
+  a?: T | string,
+  b?: T
+) {
+  // first overload
+  if (a === undefined) {
+    return (prefix: string) => _mkdtemp(prefix, "Buffer");
+  }
+
+  if (b === undefined) {
+    const as = a as T;
+    return (prefix: string) => _mkdtemp(prefix, as);
+  }
+
+  const prefix = a as string;
+  const as = b as T;
+  return _mkdtemp(prefix, as);
 }
