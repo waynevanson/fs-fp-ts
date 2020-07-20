91cfa5a0dc9d26d1f2f9ce58d54719ca33ffd99d
Wayne Van Son <waynevanson@gmail.com>
2020-07-20 22:50:10 +1000

    tests:  append-file

---
+++ src/append-file.spec.ts
@@ -0,0 +1,72 @@
+import * as fc from "fast-check";
+import { either as E, readerTaskEither as RTE } from "fp-ts";
+import { pipe } from "fp-ts/lib/function";
+import * as path from "path";
+import * as fs from ".";
+import {
+  FileArgsDeps,
+  fixturesDir,
+  rReadFile,
+  rUnlink,
+  rWriteFile,
+} from "../test-utils";
+import { TaskEitherNode } from "./util/types/fp";
+
+/**
+ * @summary
+ * This is basically the buildup and teardown,
+ * and rReadFile is injectable!
+ */
+const subject = <T>(rAppendFile: (deps: FileArgsDeps) => TaskEitherNode<T>) =>
+  pipe(
+    rWriteFile,
+    RTE.chainFirst(() => rAppendFile),
+    RTE.chain(() => rReadFile),
+    RTE.chainFirst(() => rUnlink)
+  );
+
+const table: Array<{
+  name: string;
+  property: (deps: FileArgsDeps) => TaskEitherNode;
+}> = [
+  {
+    name: "overload one without options appends files correctly",
+    property: ({ dir, content }) => fs.appendFile()(content)(dir),
+  },
+  {
+    name: "overload one with options appends files correctly",
+    property: ({ dir, content }) => fs.appendFile({})(content)(dir),
+  },
+  {
+    name: "overload two without options appends files correctly",
+    property: ({ dir, content }) => fs.appendFile(content)(dir),
+  },
+  {
+    name: "overload two with options appends files correctly",
+    property: ({ dir, content }) => fs.appendFile(content, {})(dir),
+  },
+  {
+    name: "overload three without options appends files correctly",
+    property: ({ dir, content }) => fs.appendFile(dir, content),
+  },
+  {
+    name: "overload three without options appends files correctly",
+    property: ({ dir, content }) => fs.appendFile(dir, content, {}),
+  },
+];
+
+describe.each(table)(fs.appendFile.name, ({ name, property }) => {
+  test(name, async () => {
+    await fc.assert(
+      fc.asyncProperty(fc.hexaString(), fc.string(), async (dir, content) => {
+        const newContent = content + content;
+        const input = {
+          dir: path.resolve(fixturesDir, "append-file-overloads" + dir),
+          content,
+        };
+        const result = await subject(property)(input)();
+        expect(result).toStrictEqual(E.right(newContent));
+      })
+    );
+  });
+});
