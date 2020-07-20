8d5d0494f56ecfdf995d95432d60ce0e9c627950
Wayne Van Son <waynevanson@gmail.com>
2020-07-20 21:33:41 +1000

    tests remove old access tests

--- src/access.test.ts
+++ src/access.test.ts
@@ -1,21 +1,5 @@
-import {
-  option as O,
-  taskEither as TE,
-  task as T,
-  readerTaskEither as RTE,
-  readerTask as RT,
-  either as E,
-  reader as R,
-} from "fp-ts";
-import { pipe } from "fp-ts/lib/function";
-import * as path from "path";
-import * as fs from ".";
-import { fixturesDir } from "../test-utils";
 import * as fc from "fast-check";
-import { FileMode } from "./util/types/file-permissions";
 import { AccessMode } from "./access";
-import { sequenceT } from "fp-ts/lib/Apply";
-import * as _fs from "fs";
 
 interface Model {
   name: AccessMode;
@@ -43,67 +27,12 @@ const models: Model[] = [
   },
   {
     name: "writeable",
-    max: 0,
+    max: 4,
     success: zeroseven,
   },
   {
     name: "executable",
-    max: 0,
+    max: 7,
     success: zeroseven,
   },
 ];
-
-const through = <T>(a: T) => {
-  console.log("Debug:");
-  console.log(a);
-  return a;
-};
-
-// install fast check
-describe.each(models)(fs.access.name, ({ name, success }) => {
-  it(`access with with name: \"${name}\"`, async () => {
-    await fc.assert(
-      fc.asyncProperty(
-        success,
-        zeroseven,
-        zeroseven,
-        fc.hexa(),
-        async (a, b, c, d) => {
-          const mode = Number("0o" + a + b + c) as FileMode;
-          const filePath = path.resolve(fixturesDir, "./" + d);
-
-          const fold = RTE.fold(
-            () => RT.of<_fs.PathLike, boolean>(false),
-            () => RT.of(true)
-          );
-
-          // overloads are cooked.
-          // partial args proving painful.
-          const overloads = sequenceT(RT.readerTask)(
-            // first overload
-            pipe(fs.access([name]), fold),
-            // first overload
-            pipe(fs.access(), fold),
-            // second overload
-            pipe((pathLike: _fs.PathLike) => fs.access(pathLike, [name]), fold)
-          );
-
-          const withCleanup = pipe(
-            fs.writeFile("", { mode }),
-            RTE.chain(() =>
-              pipe(
-                overloads,
-                RT.map((a) => E.right(a))
-              )
-            ),
-            RTE.chainFirst(() => fs.unlink)
-          );
-
-          const result = await withCleanup(filePath)();
-
-          expect(result).toMatchObject(E.right([true, true, true]));
-        }
-      )
-    );
-  });
-});
