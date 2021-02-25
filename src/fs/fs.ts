406ce4af719c018822cde00154f5839fb265f1a0
Wayne Van Son <waynevanson@gmail.com>
2021-02-26 08:37:08 +1100

    deps: delete old API
    BREAKING CHANGE: The whole API has been rewritten, so nothing that worked previously should work

---
+++ src/fs/fs.ts
@@ -0,0 +1,34 @@
+import { taskEither as TE } from "fp-ts";
+import { constVoid, pipe } from "fp-ts/lib/function";
+import * as FS from "fs";
+import { Flags, PathLikeOrFD, WriteableData, WriteFileOptions } from "../types";
+import {
+  asNodeJSError,
+  extractFromTuplet,
+  handleErrors,
+  promise,
+} from "../utilities";
+
+export const readFile = (flag: Flags) => (path: PathLikeOrFD) =>
+  pipe(
+    TE.tryCatch(
+      promise<[Buffer]>((executor) => {
+        FS.readFile(path, { flag, encoding: null }, handleErrors(executor));
+      }),
+      asNodeJSError
+    ),
+    TE.map(extractFromTuplet)
+  );
+
+export const writeFile = (options: WriteFileOptions = {}) => (
+  path: PathLikeOrFD
+) => (data: WriteableData) =>
+  pipe(
+    TE.tryCatch(
+      promise<[]>((executor) => {
+        FS.writeFile(path, data, options, handleErrors(executor));
+      }),
+      asNodeJSError
+    ),
+    TE.map(constVoid)
+  );
